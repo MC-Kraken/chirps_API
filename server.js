8b123dcdce2c6bf90b557103c7b83b845907497d
Student <student@Admins-iMac.local>
2019-02-23 18:38:46 -0600

    need auth

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = '3000'
-const mysql = require('mysql')
+const port = '8000';
+const mysql = require('mysql');
+const cors = require('cors');
+const jwt = require('jsonwebtoken')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
 
 //////////CONNECTION//////////
 const pool = mysql.createPool({
@@ -16,6 +15,15 @@ const pool = mysql.createPool({
     password: 'BeGre@t2019',
     database: 'chirpr'
 });
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+
 
 //////////GET ALL USERS//////////
 app.get('/users', function (req, res) {
@@ -67,7 +75,7 @@ app.get('/chirps/:userid', function (req, res) {
 
 //////////DELETE A USER//////////
 app.delete('/users/:id', function (req, res) {
-    let query = "SELECT * FROM Users WHERE id = ?";
+    let query = "DELETE * FROM Users WHERE id = ?";
 
     pool.query(query, [req.params.id], (err, results, fields) => {
         if (!err)
@@ -116,6 +124,103 @@ app.post('/users/', function (req, res) {
             console.log(err);
     });
 });
+
+
+
+
+//////////////////////////////////////////////////////////////////MOCK START/////////////////////////////////////////////////////////
+
+const path = require('path');
+
+//////////TELLING SERVER WHERE TO LOOK FOR FILES//////////
+const pathJoin = path.join(__dirname + '/views');
+app.use(express.static(path.join(__dirname + '/views')));
+app.set('view engine', 'ejs');
+
+////////////////MOCK FORM///////////////
+app.get('/index', function (req, res) {
+    res.render(pathJoin + '/index');
+});
+
+
+//////////MOCK GET USER LOGIN//////////
+app.get('/users/login', function (req, res) {
+    let query = "SELECT * FROM Users";
+
+    pool.query(query, [req.params.id], (err, results, fields) => {
+        if (!err)
+            res.send(results);
+        else
+            console.log(err);
+    });
+});
+
+
+//////////MOCK POST//////////
+app.post('users/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            });
+        }
+    });
+});
+
+
+
+//////////MOCK LOGIN AUTH//////////
+app.post('/users/login', function (req, res) {
+
+    const user = {
+        email: 'blake@yahoo.com',
+        password: 'Birmingham'
+    }
+
+    jwt.sign({ user }, 'secretkey', { expiresIn: '60s'}, (err, token) => {
+        res.json({
+            token
+        })
+    });
+});
+
+
+
+//////////VERIFY TOKEN FUNCTION//////////
+function verifyToken(req, res, next) {
+    //////////GET AUTH HEAD VALUE//////////
+    const bearerHeader = req.headers['authorization'];
+    //////////CHECK IF HEADER IS UNDEFINED//////////
+    if (typeof bearerHeader !== 'undefined') {
+        //////////SPLIT AT THE SPACE////////// 
+        const bearer = bearerHeader.split(' ');
+        //////////GET TOKEN FROM ARRAY//////////
+        const bearerToken = bearer[1];
+        //////////SET THE TOKEN//////////
+        req.token = bearerToken;
+        //////////NEXT MIDDLEWARE//////////
+        next();
+    } else {
+        //////////FORBIDDEN//////////
+        res.sendStatus(403);
+    }
+
+
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////MOCK END//////////////////////////////////////////////////////////
+
+
+
+
+
 
 //////////CREATE A CHIRP//////////
 app.post('/chirps/', function (req, res) {
